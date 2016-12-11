31a6639299dc028fff32872c10408292b7e87b13
roastchicken <roast@thatoneprogrammerkid.com>
2016-12-11 08:35:56 -0600

    Add dimension to conveyor belt

--- main.lua
+++ main.lua
@@ -17,5 +17,8 @@ function love.draw()
   love.graphics.rectangle( "fill", -310, 0, 620, 180 ) -- floor
   love.graphics.setColor( { 151, 165, 170 } )
   love.graphics.rectangle( "fill", -200, -25, 400, 50 ) -- back of conveyor belt
+  love.graphics.rectangle( "fill", -180, 5, 400, 50 ) -- front of conveyor belt
+  love.graphics.setColor( { 26, 32, 34 } )
+  love.graphics.polygon( "fill", -200, -25, 200, -25, 220, 5, -180, 5 ) -- top of conveyor belt
   camera:detach()
 end
