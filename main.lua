e9b1beadcc08793d686e5f10ec4409a708617b38
roastchicken <roast@thatoneprogrammerkid.com>
2016-12-11 08:46:13 -0600

    Darken back of conveyor belt

--- main.lua
+++ main.lua
@@ -15,8 +15,9 @@ function love.draw()
   camera:attach()
   love.graphics.setColor( { 120, 46, 14 } )
   love.graphics.rectangle( "fill", -310, 0, 620, 180 ) -- floor
-  love.graphics.setColor( { 151, 165, 170 } )
+  love.graphics.setColor( { 128, 136, 139 } )
   love.graphics.rectangle( "fill", -200, 10, 400, 70 ) -- back of conveyor belt
+  love.graphics.setColor( { 151, 165, 170 } )
   love.graphics.rectangle( "fill", -160, 70, 400, 70 ) -- front of conveyor belt
   love.graphics.setColor( { 26, 32, 34 } )
   love.graphics.polygon( "fill", -200, 10, 200, 10, 240, 70, -160, 70 ) -- top of conveyor belt
