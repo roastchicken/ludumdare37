d0ac65c0ada43563fd6f375f6eeb44597992eeac
roastchicken <roast@thatoneprogrammerkid.com>
2016-12-11 08:43:58 -0600

    Move and resize conveyor belt

--- main.lua
+++ main.lua
@@ -16,9 +16,9 @@ function love.draw()
   love.graphics.setColor( { 120, 46, 14 } )
   love.graphics.rectangle( "fill", -310, 0, 620, 180 ) -- floor
   love.graphics.setColor( { 151, 165, 170 } )
-  love.graphics.rectangle( "fill", -200, -25, 400, 50 ) -- back of conveyor belt
-  love.graphics.rectangle( "fill", -180, 5, 400, 50 ) -- front of conveyor belt
+  love.graphics.rectangle( "fill", -200, 10, 400, 70 ) -- back of conveyor belt
+  love.graphics.rectangle( "fill", -160, 70, 400, 70 ) -- front of conveyor belt
   love.graphics.setColor( { 26, 32, 34 } )
-  love.graphics.polygon( "fill", -200, -25, 200, -25, 220, 5, -180, 5 ) -- top of conveyor belt
+  love.graphics.polygon( "fill", -200, 10, 200, 10, 240, 70, -160, 70 ) -- top of conveyor belt
   camera:detach()
 end
