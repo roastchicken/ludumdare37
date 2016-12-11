b6b80b0feb05e669e88dcc7678ae90980ba9c95c
roastchicken <roast@thatoneprogrammerkid.com>
2016-12-11 10:33:11 -0600

    Replace polygon draw call with drawConveyorBelt()
    Replace extraneous love.graphics.polygon() with drawConveyorBelt()
    
    drawConveyorBelt() was created to be used as the stencil for masking
    the conveyor belt lines, as such it has the same shape as the conveyor
    belt so it can replace the previous polygon draw call which was placed
    in drawConveyorBelt().

--- main.lua
+++ main.lua
@@ -29,7 +29,7 @@ function love.draw()
   love.graphics.setColor( { 151, 165, 170 } )
   love.graphics.rectangle( "fill", -160, 70, 400, 70 ) -- front of conveyor belt
   love.graphics.setColor( { 26, 32, 34 } )
-  love.graphics.polygon( "fill", -200, 10, 200, 10, 240, 70, -160, 70 ) -- top of conveyor belt
+  drawConveyorBelt()
   love.graphics.setColor( { 12, 15, 16 } )
   
   love.graphics.stencil( drawConveyorBelt, "replace", 1 )
