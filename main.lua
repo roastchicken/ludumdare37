42598f98c1e8fced2f60fd724823decf788a582e
roastchicken <roast@thatoneprogrammerkid.com>
2016-12-11 08:29:58 -0600

    Differentiate rectangles with comments

--- main.lua
+++ main.lua
@@ -14,8 +14,8 @@ end
 function love.draw()
   camera:attach()
   love.graphics.setColor( { 120, 46, 14 } )
-  love.graphics.rectangle( "fill", -310, 0, 620, 180 )
+  love.graphics.rectangle( "fill", -310, 0, 620, 180 ) -- floor
   love.graphics.setColor( { 151, 165, 170 } )
-  love.graphics.rectangle( "fill", -200, -25, 400, 50 )
+  love.graphics.rectangle( "fill", -200, -25, 400, 50 ) -- back of conveyor belt
   camera:detach()
 end
