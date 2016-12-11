1ad33bb295ff899aaeed8f9a9e22cd02ad27d971
roastchicken <roast@thatoneprogrammerkid.com>
2016-12-11 08:24:22 -0600

    Change scene

--- main.lua
+++ main.lua
@@ -2,7 +2,7 @@ function love.load()
   love.window.setMode( 620, 360, { x = 1300, y = 245 } )
   love.window.setTitle( "Ludum Dare 37 Game" )
   lurker = require( "lurker" )
-  love.graphics.setBackgroundColor( { 42, 201, 184 } )
+  love.graphics.setBackgroundColor( { 94, 36, 11 } )
   Camera = require( "camera" )
   camera = Camera( 0, 0 )
 end
@@ -13,9 +13,9 @@ end
 
 function love.draw()
   camera:attach()
-  love.graphics.setColor( { 255, 255, 255 } )
-  love.graphics.rectangle( "fill", -25, -50, 50, 100 )
-  love.graphics.setColor( { 21, 152, 11 } )
-  love.graphics.rectangle( "fill", -2000, 50, 4000, 2000 )
+  love.graphics.setColor( { 120, 46, 14 } )
+  love.graphics.rectangle( "fill", -310, 0, 620, 180 )
+  love.graphics.setColor( { 151, 165, 170 } )
+  love.graphics.rectangle( "fill", -200, -25, 400, 50 )
   camera:detach()
 end
