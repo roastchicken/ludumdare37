0fdb7be7f8c4b6918552b3f8aedaf5da14268504
roastchicken <roast@thatoneprogrammerkid.com>
2016-12-10 20:59:24 -0600

    Add lurker

--- main.lua
+++ main.lua
@@ -1,10 +1,11 @@
 function love.load()
   love.window.setMode( 1280, 720, { x = 0, y = 0, vsync = false } )
   love.window.setTitle( "Ludum Dare 37 Game" )
+  lurker = require( "lurker" )
 end
 
 function love.update( dt )
-  
+  lurker.update()
 end
 
 function love.draw()
